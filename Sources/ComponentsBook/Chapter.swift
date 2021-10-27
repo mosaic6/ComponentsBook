927645d8a881caa61b47cd2c0e0f9be18b0e5eaa
Joshua Walsh <joshuatwalsh@gmail.com>
2021-10-26 21:21:11 -0400

    Update init

--- Sources/ComponentsBook/Chapter.swift
+++ Sources/ComponentsBook/Chapter.swift
@@ -12,10 +12,18 @@ import SwiftUI
 public struct Chapter: Identifiable {
     public let id = UUID()
     public var type: MainTypes
-    public var title: String
+    public var title: String?
     public var description: String?
     public var view: AnyView
     public let children: [Chapter]?
+
+    public init(type: MainTypes, title: String?, description: String?, view: AnyView, children: [Chapter]?) {
+        self.type = type
+        self.title = title
+        self.description = description
+        self.view = view
+        self.children = children
+    }
 
     @ViewBuilder func makeView() -> some View {
         VStack {
